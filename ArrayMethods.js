e472fbbd6fdb7205ad4745ce23baebafbca29169
AbhishekB27 <akstyles27@gmail.com>
2022-05-16 22:30:53 +0530

    add filter and reduce methods

--- ArrayMethods.js
+++ ArrayMethods.js
@@ -1,3 +1,11 @@
 console.log("DAY-27");
 const arr = [10,20,30,40,50];
-arr.forEach(val=>console.log(val))
+
+arr.forEach(val=>console.log("Value: " + val));
+arr.filter(val=> val%3 == 0).forEach((value)=>{console.log("Filter value is: " + value)});
+let sum = arr.reduce((preVal,nextVal)=>{
+    let total = preVal + nextVal;
+    console.log("preVal: " + preVal + " : " + "nextVal: " + nextVal)
+    return total;
+});
+console.log(sum);
