89334c3c8118d3b0a047e6a003603db1616b6a90
Abhishek Bahuguna <96983555+AbhishekB27@users.noreply.github.com>
2022-06-02 20:51:25 +0530

    Delete App.js

--- App.js
+++
@@ -1,8 +0,0 @@
-const url = 'https://dummyjson.com/products/';
-const response = fetch(url)
-                      .then((data)=>{
-                          console.log(data)
-                          return data.json()
-                      }).then((result)=>{
-                          console.log(result)
-                      }).catch(error=>console.log(error))
