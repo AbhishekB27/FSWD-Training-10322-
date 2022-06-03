7dcc8096c54c08d28179b3e452fc2e11c78d985e
AbhishekB27 <akstyles27@gmail.com>
2022-06-03 19:24:46 +0530

    os modules

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 // console.log(module)// it show current directory object
 // calculation eg:
 const val = require('./calculation')
+const os= require('os')
 const x =10;
 const z=5;
 // console.log("ADD:(10+5) = " + val.add(x,z))
@@ -15,3 +16,19 @@ const z=5;
 // console.log("MUL:(10*5) = " + val.mul(x,z))
 // console.log("DIV:(10/5) = " + val.div(x,z))
 console.log(val('Abhishek'))
+console.log(os.homedir())
+console.log(os.arch())
+console.log(os.userInfo())
+console.log(os.cpus())
+console.log(os.platform())
+console.log(os.freemem())
+console.log(os.hostname())
+console.log(os.networkInterfaces())
+console.log(os.release())
+
+
+
+
+
+
+
