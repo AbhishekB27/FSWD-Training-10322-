9bd952079f4a5519973503655801e9d2d8a51a70
AbhishekB27 <akstyles27@gmail.com>
2022-06-07 20:12:45 +0530

    learn about node js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const http = require('http')
-
-const server = http.createServer((req,res)=>{
-    // console.log(req.headers)
-    // console.log(req.url)
-    // console.log("You made a req")
-    res.write("<h1>PING from server</h1>")
-    res.end();
-})
-
-server.listen(8080)
-
-
-
-
-
-
+console.log("hi abhishek")
+console.log("how are you")
 
