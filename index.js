fbe536225984bc163d67a346ce40aa1efc6aa814
AbhishekB27 <akstyles27@gmail.com>
2022-06-08 21:24:50 +0530

    Express js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-console.log("hi abhishek")
-console.log("how are you")
+const express = require('express')
+const { path } = require('express/lib/application')
+const fs = require('fs')
+const app = express()
+app.get('/',(req,res)=>{
+    res.send('response')
+    console.log(req.query)
+})
+
+app.get('/About',(req,res)=>{
+    res.send('About Page')
+})
+app.get('/Home',(req,res)=>{
+    res.send('Home Page')
+})
+console.log(app)
+app.listen(8080,()=>{
+    console.log(`server started at port: ${8080}`)
+})
 
